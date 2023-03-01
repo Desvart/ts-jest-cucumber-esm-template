13d0bb767f06492e4d23a1ed3ec4d20fcc3a016a
Derek Svart <derek.svart@gmail.com>
2023-03-01 01:50:05 +0100

    NEW FEATURES * Add cucumberJs to the template. Work in CLI mode or through Webstorm IDE Cucumber plugin.
    Signed-off-by: Derek Svart <derek.svart@gmail.com>

---
+++ src/module-X/feature/calculator.feature
@@ -0,0 +1,26 @@
+Feature: As a user, I want to add and substract two numbers
+
+  Scenario: Add two positive numbers
+    Given
+    When I add 2 and 3
+    Then the result of the addition is 5
+
+  Scenario: Add a number and 0
+    Given
+    When I add 2 and 0
+    Then the result of the addition is 2
+
+  Scenario: Add a positive number and a negative number
+    Given
+    When I add 2 and -5
+    Then the result of the addition is -3
+
+  Scenario: Add two negative numbers
+    Given
+    When I add -2 and -2
+    Then the result of the addition is -4
+
+  Scenario: Sub two positive numbers
+    Given
+    When I sub 2 and 2
+    Then the result of the substraction is 1
---
+++ src/module-X/test/calculator.steps.ts
@@ -0,0 +1,29 @@
+import assert from 'assert';
+import { When, Then } from '@cucumber/cucumber';
+import { Calculator } from '../domain/entity/calculator';
+
+When(/^I add (-?\d+) and (-?\d+)$/, function (a: number, b: number) {
+  this.a = a;
+  this.b = b;
+});
+
+Then(
+  /^the result of the addition is (-?\d+)$/,
+  function (expectedResponse: number) {
+    const calc = new Calculator();
+    assert.equal(calc.add(this.a, this.b), expectedResponse);
+  }
+);
+
+When(/^I sub (-?\d+) and (-?\d+)$/, function (a: number, b: number) {
+  this.a = a;
+  this.b = b;
+});
+
+Then(
+  /^the result of the substraction is (-?\d+)$/,
+  function (expectedResponse: number) {
+    const calc = new Calculator();
+    assert.equal(calc.sub(this.a, this.b), expectedResponse);
+  }
+);
